07036165008339f5ea8153f8a024d7c7c6b5f712
= <rickycharpentier@gmail.com>
2020-03-04 01:20:34 -0500

    updates

--- config/settings.py
+++ config/settings.py
@@ -63,7 +63,7 @@ CELERYBEAT_SCHEDULE = {
         'task': 'app.blueprints.api.tasks.generate_drops',
         # 'schedule': crontab(hour="*/1") # every hour
         # 'schedule': crontab(minute="*/1") # every minute
-        'schedule': crontab(hour=1, minute=20) # every night at midnight
+        'schedule': crontab(hour=6, minute=25) # every night at midnight
     },
 }
 
