bd791c92da2313a7c4c356dbb0c7642f0e6feb96
Ricky <rickycharpentier@gmail.com>
2020-03-10 08:13:23 -0700

    updates

--- config/settings.py
+++ config/settings.py
@@ -70,7 +70,7 @@ CELERYBEAT_SCHEDULE = {
     # Attempt to order the domains
     'order_domains': {
         'task': 'app.blueprints.api.tasks.order_domains',
-        'schedule': timedelta(seconds=30) # every 30 seconds after the previous one completes
+        'schedule': timedelta(seconds=10) # every 10 seconds after the previous one completes
     },
 
     # Delete successfully paid backorders
